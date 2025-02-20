081c583fca1057d3cbd45b3ea18754dbd4497fa9
jquie <jacquelynphan03@gmail.com>
2025-02-20 18:17:25 +0000

    not done

--- Main.java
+++ Main.java
@@ -5,9 +5,51 @@ public class Main
 {
   public static void main(String[] args)
   {
+    /*
+    //Classwork - create an arrat of ints, fill it with stuff and loop through it, print every value
+    int[] jennys = {8,7,9,5,6,4,5,3};
+    for (int i = 0; i < jennys.length; i++)
+    {
+      System.out.println(jennys[i]);
+    }
+    //can also use enhanced loop
+    for (int n : jennys)
+    {
+      System.out.println(n);
+    }
+    //Do for ArrayList
+    ArrayList<Integer> foo = new ArrayList<Integer>();
+    foo.add(new Integer(1));
+    foo.add(new Integer(2));
+    foo.add(new Integer(5));
+    foo.add(new Integer(3));
+
+    for (int i = 0; i < foo.size(); i++)
+    {
+      System.out.println(foo.get(i));
+    }
+
+    // enhanced for loop
+    for (Integer N : foo)
+    {
+      System.out.print(N); // toString()
+      System.out.println(N.intValue()); // intValue returns the int value instead of the class
+    }
+    */
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(1);
+    list.add(2);
+    list.add(4);
+    list.add(5);
+    list.add(7);
+    System.out.println(getEvens(list));
+    System.out.println(highestNum(list));
+
+    //Problem 1
     Scanner sc = new Scanner(System.in);
     String input = "";
-
+    
     while (!input.equals("STOP"))
     {
       System.out.println("infinite loop! replace with your code");
@@ -17,12 +59,28 @@ public class Main
   // Problem 2 - HighestNum
   public static Integer highestNum(ArrayList<Integer> arr)
   {
-    return null;
+    int max = (arr.get(0)).intValue();
+    for (Integer n : arr)
+    {
+      if (n > max)
+      {
+        max = n.intValue();
+      }
+    }
+    return max;
   }
 
   // Problem 3 - getEvens
   public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
   {
-    return null;
+    ArrayList<Integer> ans = new ArrayList<Integer> ();
+    for (Integer n : arr)
+    {
+      if (n % 2 == 0)
+      {
+        ans.add(n);
+      }
+    }
+    return ans;
   }
 }
